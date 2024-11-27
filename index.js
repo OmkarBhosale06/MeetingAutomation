8f3389220aa5978b056ca9a2f553b94488131bd3
omkar bhosale <omkarbhosale0606@gmail.com>
2024-11-28 01:06:54 +0530

    uncommenting code

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ const port = 3000;
 app.use(bodyParser.json());
 
 await joinGoogleMeet()
-// app.post('/join-meet', async (req, res) => {
-//     // Get the meet URL, email, and password from the request body
-//     const { meetUrl, email, password } = req.body;
+app.post('/join-meet', async (req, res) => {
+    // Get the meet URL, email, and password from the request body
+    const { meetUrl, email, password } = req.body;
   
-//     if (!meetUrl || !email || !password) {
-//       return res.status(400).send('Please provide meetUrl, email, and password.');
-//     }
+    if (!meetUrl || !email || !password) {
+      return res.status(400).send('Please provide meetUrl, email, and password.');
+    }
   
-//     try {
-//       console.log('Request received to join Google Meet...');
+    try {
+      console.log('Request received to join Google Meet...');
       
-//       // Call the function from joinMeet.js
-//       await joinGoogleMeet(meetUrl, email, password);
+      // Call the function from joinMeet.js
+      await joinGoogleMeet(meetUrl, email, password);
       
-//       return res.status(200).send('Successfully joined the Google Meet!');
-//     } catch (error) {
-//       console.error('Error joining Google Meet:', error);
-//       return res.status(500).send('Failed to join Google Meet.');
-//     }
-//   });
+      return res.status(200).send('Successfully joined the Google Meet!');
+    } catch (error) {
+      console.error('Error joining Google Meet:', error);
+      return res.status(500).send('Failed to join Google Meet.');
+    }
+  });
   
   // Start the Express server
   app.listen(port, () => {
